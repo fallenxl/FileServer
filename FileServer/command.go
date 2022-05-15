8a77c96c7aac01e2de5104320d58b2eda364f4a0
Axl Santos <64768381+fallenxl@users.noreply.github.com>
2022-05-15 14:02:14 -0600

    Update command.go

--- FileServer/command.go
+++ FileServer/command.go
@@ -3,12 +3,12 @@ package main
 type commandID int
 
 const (
-	cmd_user     commandID = iota // funciona como iterador = 0
-	cmd_list                      //1
-	cmd_suscribe                  //2
-	cmd_members                   //3
-	cmd_send                      //4
-	cmd_close                     //5
+	cmd_user     commandID = iota + 1 // funciona como iterador = 1
+	cmd_list                          //2
+	cmd_suscribe                  	  //3
+	cmd_members                       //4
+	cmd_send                          //5
+	cmd_close                         //6
 )
 
 type command struct {
